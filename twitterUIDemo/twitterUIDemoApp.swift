afc22cd356406a915db1e790a4f0b72d3eda7c26
Prabhat Pankaj <prabhatpankaj@Prabhats-MacBook-Pro.local>
2023-11-01 15:59:21 +0530

    Initial commit

---
+++ twitterUIDemo/twitterUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterUIDemoApp.swift
+//  twitterUIDemo
+//
+//  Created by Prabhat Pankaj on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct twitterUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
